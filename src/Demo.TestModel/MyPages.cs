bbe19b972e1a383bc999c9111b727d41ab686b70
Dmytro Zharii <dzhariy@gmail.com>
2014-01-09 23:47:45 +0200

    Added code documentation / Doxygen

--- src/Demo.TestModel/MyPages.cs
+++ src/Demo.TestModel/MyPages.cs
@@ -6,22 +6,41 @@ using Demo.TestModel.PageDeclarations;
 
 namespace Demo.TestModel
 {
+    /**   
+        The class `MyPages` is an entry point for your PageObjects.   \n
+        This is a Factory class, which is accessible from any place of the code.  \n
+        Allows to call PegeObject actions from tests as well as from another PaeObjects   \n
+    */
     public static class MyPages
     {
 
-        // This method can be extended to support additional logic for all pages, 
-        // for instance, switch to the frame or window when it would be required or cache 
-        // the created pageobject instance
+        /**
+            This method can be extended to support additional logic for all pages, 
+            for instance, switch to the frame or window when it would be required or cache 
+            the created pageobject instance 
+        */
         public static T GetPage<T>() where T : MyPageBase, new()
         {
             T page = new T();
             return page;
         }
 
+        /**
+            Adding new pages
+            ----------------
+            For instance, you have defined a new PageObject with name `UserProfilePage` \n
+            In order to have a convenient access to this page from any place of the test automation code, like this one: 
 
-        // This is an example for adding pages into MyPages. 
-        // Please, notice the property name is same as returning type; 
-        //            /* TYPE       */ /* NAME       */
+            > MyPages.UserProfilePage.DoSomeAction(“param1”);
+
+            Put the line in the following format to the list below: 
+
+            >  public static UserProfilePage UserProfilePage { get { return GetPage<UserProfilePage>(); } }
+
+            Please, don't be confused because the property name and returning type are same. It is allowed in C#, but, anyway, you can give any name for the property. 
+        */
+
+        // Example:
         public static DummyExamplePage DummyExamplePage { get { return GetPage<DummyExamplePage>(); } }
         public static EmptyPage        EmptyPage        { get { return GetPage<EmptyPage>(); } }
 
--- src/SWD.Core/Configuration/Config.cs
+++ src/SWD.Core/Configuration/Config.cs
@@ -6,6 +6,49 @@ using System.Configuration;
 
 namespace Swd.Core.Configuration
 {
+    /// <summary>
+    ///  Config -- Core Configuration Class
+    /// </summary>
+    
+    #region Documentation
+    /// <summary> 
+    /// Framework Configuration Files
+    /// -----------------------------
+    /// The configuration class in the Swd.Starter allows to change some values without project recompilation, such as:
+    ///
+    /// * Selecting WebBrowser
+    /// * Setting the main application  URLs and others;
+    ///
+    /// The configuration sub-module is based on standard .NET Framework XML configuration files with several tweaks. 
+    ///
+    /// In the project Src folder, you can find the following files:
+    /// * App.config – contains a technical configuration and includes the file Config.config. (See line <appSettings configSource="Config.config"/>)
+    /// * Config.config – contains user-defined settings.
+    ///
+    /// The class `Config` read the configuration values from the class `Config.config` \n
+    ///
+    ///
+    /// Those two configuration files are shared between all the projects. 
+    ///
+    /// How to properly add configuration files into  the new project
+    /// -----------------------------------------------------------------------------------------
+    /// For instance, you have created new testing projects `MyApp.TestModel` and MyApp.Tests. 
+    ///
+    /// In order to add Global Configuration Files support, please follow the instructions below: 
+    ///
+    /// 1. Open Right-click context menu for project `MyApp.TestModel` in the Solution Explorer
+    /// 2. Pick item Add → Existing Item...
+    /// 3. In the Open File fialog, find the file `App.config` in the folder `SWD.Starter\src`
+    /// 4. Select the file, but do not press button “Add” yet
+    /// 5. Clich on the right side of the Add button in order to Invoke Add popup menu from
+    /// 6. Pick “Add as a Link”
+    /// 7. Recompile the project
+    ///
+    /// Repeat the steps for `MyApp.Tests`
+    /// <summary> 
+
+    #endregion
+
     public class Config
     {
         public static bool wdIsRemote
@@ -22,6 +65,7 @@ namespace Swd.Core.Configuration
                 return ConfigurationManager.AppSettings["swdRemoteUrl"];
             }
         }
+
         public static string swdBrowserType
         {
             get
@@ -29,5 +73,28 @@ namespace Swd.Core.Configuration
                 return ConfigurationManager.AppSettings["swdBrowserType"];
             }
         }
+
+        /// <summary>
+        /// Add your own configuration parameters
+        /// -------------------------------------
+        /// Quick instructions: \n
+        ///
+        /// 1. Open the file Config.config
+        /// 2. Add the following xml line: 
+        ///    > <add key="yourParameterName" value="AnyValue"/>
+        /// 3. Create a property with name “yourParameterName” which reads configuration file key yourParameterName
+        /// </summary>
+        /// 
+        /// Put your configuration settings below:
+
+        public static string yourParameterName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["yourParameterName"];
+            }
+        }        
+
     }
+
 }
